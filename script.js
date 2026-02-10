4493eb935701c7060f87b9180bd004dfb337117a
hrutuja864 <hrutujameshram864@gmail.com>
2026-02-10 19:19:21 +0530

    Updated portfolio design and transitions

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = ()=>{
-    menuIcon.classList.toggle(bx-x);
-    navbar.classList.toggle('active')
-}
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
 
+/* SCROLL ANIMATION */
+const scrollElements = document.querySelectorAll(".scroll-animate");
+
+const scrollAnimation = () => {
+  scrollElements.forEach(el => {
+    const elementTop = el.getBoundingClientRect().top;
+    const visiblePoint = 120;
+
+    if (elementTop < window.innerHeight - visiblePoint) {
+      el.classList.add("show");
+    } else {
+      el.classList.remove("show");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollAnimation);
+window.addEventListener("load", scrollAnimation);
